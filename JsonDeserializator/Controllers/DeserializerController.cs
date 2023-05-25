1f348e859c866eb0b62acdd598d79e4de8a3d482
Dmitry Ivanov <diemon_2000@mail.ru>
2023-05-25 14:33:09 +0300

    Добавьте файлы проекта.

--- JsonDeserializator/Controllers/DeserializerController.cs
+++ JsonDeserializator/Controllers/DeserializerController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using JsonDeserializator.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JsonDeserializator.Controllers
@@ -17,6 +18,7 @@ namespace JsonDeserializator.Controllers
         //Открывает окно для загрузки JSON файла для его десериализации, отправляет список уже загруженных ранее файлов. 
         public ActionResult Index()
         {
+            SingleFileModel model = new SingleFileModel();
             var path = Environment.CurrentDirectory + @"\UploadedJsonFiles";
             DirectoryInfo dir = new DirectoryInfo(path);
 
@@ -33,12 +35,96 @@ namespace JsonDeserializator.Controllers
             return View(list);
         }
 
-        public async Task<IActionResult> Upload(IFormFile file)
+        public IActionResult Upload()
         {
-            string filePath = Environment.CurrentDirectory + @"\UploadedJsonFiles";
-            string block = "Этот метод работает";
-            return View(block);
+            SingleFileModel model = new SingleFileModel();
+            return View(model);
         }
+
+        public IActionResult MultiFile()
+        {
+            MultipleFilesModel model = new MultipleFilesModel();
+            return View(model);
+        }
+
+        //Сохраняет файл, пока что скрыто, т.к. есть проблема передачи файла.
+        [HttpPost]
+        public IActionResult UploadSave(SingleFileModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.IsResponse = true;
+
+                var path = Environment.CurrentDirectory + @"\UploadedJsonFiles";
+
+                //create folder if not exist
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+
+                //get file extension
+                FileInfo fileInfo = new FileInfo(model.File.FileName);
+                string fileName = model.FileName + fileInfo.Extension;
+
+                string fileNameWithPath = Path.Combine(path, fileName);
+
+                using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
+                {
+                    model.File.CopyTo(stream);
+                }
+                model.IsSuccess = true;
+                model.Message = "File upload successfully";
+            }
+            return View("Index", model);
+        }
+
+        //Мульти-загрузка файлов, сейчас работает и загружает файлы в директорию, но необходимо написать проверку типа isExist
+        [HttpPost]
+        public IActionResult MultiUpload(MultipleFilesModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.IsResponse = true;
+                if (model.Files.Count > 0)
+                {
+                    foreach (var file in model.Files)
+                    {
+
+                        var path = Environment.CurrentDirectory + @"\UploadedJsonFiles";
+
+                        //create folder if not exist
+                        if (!Directory.Exists(path))
+                            Directory.CreateDirectory(path);
+
+
+                        string fileNameWithPath = Path.Combine(path, file.FileName);
+
+                        using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
+                        {
+                            file.CopyTo(stream);
+                        }
+                    }
+                    model.IsSuccess = true;
+                    model.Message = "Files upload successfully";
+                }
+                else
+                {
+                    model.IsSuccess = false;
+                    model.Message = "Please select files";
+                }
+            }
+            return View("MultiFile", model);
+        }
+        ////Метод-заглушка для загрузки
+        //public async Task<IActionResult> Upload(IFormFile file)
+        //{
+        //    string filePath = Environment.CurrentDirectory + @"\UploadedJsonFiles";
+        //    string block = "Этот метод работает";
+        //    return View(block);
+        //}
+
+
+        //Контроллер, выводящий список файлов по их сокращённому названию и дающий метод вывода данных по id
+
         //Post-метод index проводит RedirrectToAction(GetTableView) в метод записи файла + проводит валидацию на то, есть ли такой файл уже в системе
 
         public async Task<IActionResult> OnPostUploadAsync(List<IFormFile> files)
